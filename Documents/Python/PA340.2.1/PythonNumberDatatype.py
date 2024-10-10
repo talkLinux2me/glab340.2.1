25befbadb7b49bf0957b6903f86c632145dc3521
talkLinux2me <167791277+talkLinux2me@users.noreply.github.com>
2024-10-10 12:07:24 -0400

    finished lab

---
+++ Documents/Python/PA340.2.1/PythonNumberDatatype.py
@@ -0,0 +1,67 @@
+#Write a Python program that declares two integer variables, assigns an integer to each, and adds them together. 
+# Assign the sum to a variable. Print out the result.
+a = 4
+b = 6
+c = a+b
+print (c)
+
+#Write a Python program that declares two float variables, assigns a number to each, and adds them together.
+# Assign the sum to a variable. Print out the result.
+d = 4.5
+e = 7.5
+f = d + e
+print (f)
+
+# Write a Python program that declares an integer variable and a float variable, assigns numbers to each, and adds them together. 
+# Assign the sum to a variable. Print out the result. What variable type must the sum be?
+#  (Hint: The variable type of the sum must be a float).
+g = 10
+h = 12.2
+i = g + h 
+print (i)
+
+# Write a Python program that declares two integer variables, assigns an integer to each, 
+# and divides the larger number by the smaller number. 
+# Assign the result to a variable. Print out the result.
+j = 20
+k = 2
+l = j/k
+print (l)
+
+# Write a Python program that declares two float variables, assigns a number to each, 
+# and divides the larger by the smaller number. 
+# Assign the result to a variable. Print out the result. 
+m = 21.5
+n = 24.7
+o = n/m
+print (o)
+
+# Write a program where you create three variables that represent products at a cafe. 
+# The products could be beverages such as `coffee,` `cappuccino,` `espresso,` `green tea,` etc. 
+# Assign prices to each product.
+
+coffee = 2.50
+tea = 3.25
+espresso = 4.00
+
+# Create two more variables called subtotal and totalSale, and complete an “order” for three items of the first product, 
+# four items of the second product, and two items of the third product. Add them all together to calculate the subtotal.
+#  Create a variable called SALES_TAX and add sales tax to the subtotal to obtain the totalSale amount.
+subtotal = ()
+totalSale = ()
+
+order1 = coffee * 3
+order2= tea * 4
+order3 = espresso * 2
+subtotal = order1 + order2 + order3
+print(subtotal)
+
+sales_tax = .08
+totalSale = subtotal + sales_tax
+print(totalSale)
+
+# Write a Python program that calculates the area of a circle based on the radius = 63. 
+# To calculate the area of a circle based on the radius, we can use the following formula:
+radius = 63
+area_of_a_circle = 3.14 * radius
+print (area_of_a_circle)
