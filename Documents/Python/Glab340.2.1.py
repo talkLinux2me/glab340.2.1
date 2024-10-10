95126ffeab0692924b59e84a10e0d9a03b381ec1
talkLinux2me <167791277+talkLinux2me@users.noreply.github.com>
2024-10-10 11:34:52 -0400

    glab340.2.1

---
+++ Documents/Python/Glab340.2.1.py
@@ -0,0 +1,16 @@
+#Membership Operators
+x = ["Rose", "Lotus"]
+print('Is value Present?', "Rose" in x)
+print(' Is value not Present?', "Riya" not in x)
+
+x = 'Hello world'
+y = {1:'a', 2:'b'}
+
+# check if 'H' is present in x string
+print('H' in x)  # prints True
+# check if 'hello' is present in x string
+print('hello' not in x)  # prints True
+# check if '1' key is present in y
+print(1 in y)  # prints True
+# check if 'a' key is present in y
+print('a' in y)  # prints False
